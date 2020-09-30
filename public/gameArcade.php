47b44165fc6c075021ee1096955245f24a1dc1cd
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-09-30 14:49:44 +0200

    Skeleton OK

--- public/gameArcade.php
+++ public/gameArcade.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+    <title>Arcade Games</title>
 </head>
 <body>
+<?php include('_header.php'); ?>
 
+    <h2>Arcade games</h2>
+    <img src="assets/images/icone/arcade.png" class="gameIcon" alt="icon of a gamepad">
+
+    <section class="container gameCartouche">
+        <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture" alt="gameplay picture of flexbox-zombies">
+        <p>lorem ipsum</p>
+        <button class="fakePlayButton">PLAY</button>
+    </section>
+
+<?php include('_footer.php'); ?>
 </body>
 </html>
