57cd51921f9a0a0496894c2c8fe8714ea1336920
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-01 12:31:22 +0200

    2 new pages creation (education and reflexion)

---
+++ public/gameReflexion.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+    <title>Arcade Games</title>
+</head>
+<body>
+<?php include('_header.php'); ?>
+    <main>
+        <img src="assets/images/icone/arcade.png" class="gameIcon" alt="icon of a gamepad">
+        <h2>Arcade games</h2>
+        <section  class="gamePage">
+                <!-- Cartouche block 1 start-->
+            <a href="https://mastery.games/flexboxzombies/" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <section class="container gameCartouche">
+                    <h3 class="gameTittle">Flexbox Zombies</h3>
+                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                        <br>
+                        <button class="fakePlayButton">PLAY</button>
+                    </p>
+                </section>
+            </a>
+            <!-- Cartouche block 1 end-->
+            <!-- Cartouche block 1 start-->
+            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <section class="container gameCartouche">
+                    <h3 class="gameTittle">Flexbox Zombies</h3>
+                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                        <br>
+                        <button class="fakePlayButton">PLAY</button>
+                    </p>
+                </section>
+            </a>
+            <!-- Cartouche block 1 end-->
+            <!-- Cartouche block 1 start-->
+            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <section class="container gameCartouche">
+                    <h3 class="gameTittle">Flexbox Zombies</h3>
+                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                        <br>
+                        <button class="fakePlayButton">PLAY</button>
+                    </p>
+                </section>
+            </a>
+            <!-- Cartouche block 1 end-->
+            <!-- Cartouche block 1 start-->
+            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <section class="container gameCartouche">
+                    <h3 class="gameTittle">Flexbox Zombies</h3>
+                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                        <br>
+                        <button class="fakePlayButton">PLAY</button>
+                    </p>
+                </section>
+            </a>
+            <!-- Cartouche block 1 end-->
+        </section>
+    </main>
+<?php include('_footer.php'); ?>
+</body>
+</html>
