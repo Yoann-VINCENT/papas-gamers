1b280f51d7c4887392440123b926cf6b8800fb43
Raphael Liere <raphael.liere@hotmail.fr>
2020-10-01 13:53:00 +0200

    Add of a container with articles in index and a css file

--- public/index.php
+++ public/index.php
@@ -40,7 +40,35 @@
     </a>
 </div>
 
-<?php include ('public/assets/ressources/_footer.php') ?>
+<p class="catch_phrase">Hey young developer ! </br> Are you boring ? Here you have some games, enjoy it !</p>
+
+
+<div class="articles">
+
+    <section class="categorie_cards">
+        <img src="assets/images/icone/Arcade_resize.png" alt="icone of arcade games">
+        <div class="text_card">
+            <h5>Arcade games</h5>
+        </div>
+    </section>
+
+    <section class="categorie_cards">
+        <img src="assets/images/icone/Education_resize.png" alt="icone of education games">
+        <div class="text_card">
+            <h5>Educatives games</h5>
+        </div>
+    </section>
+
+    <section class="categorie_cards">
+        <img src="assets/images/icone/Reflexion_resize.png" alt="icone of mind games">
+        <div class="text_card">
+            <h5>Mind games</h5>
+        </div>
+    </section>
+
+
+
+    <?php include ('public/assets/ressources/_footer.php') ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
