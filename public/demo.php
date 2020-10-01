db90052b345982be8c3df88d1e16b437e3dfc377
Florian-Rowehy <florian.rowehy@gmail.com>
2020-10-01 18:56:59 +0200

    basic working  whack-a-mole v0: fixed grid 4x4, start/end button, no sound, no score, no animation

---
+++ public/demo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="whack-a-mole.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div>
+            <div class="score-display"></div>
+            <div class="container-whack"></div>
+            <div>
+                <button class="whack-start">Launch game!</button>
+                <button class="whack-end">Stop game!</button>
+            </div>
+        </div>
+
+        <script src="./whack-a-mole.js"></script>
+    </body>
+</html>
