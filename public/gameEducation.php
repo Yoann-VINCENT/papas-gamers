a2c66754d2e921dd2061f6b28bd1561358375f1f
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-01 14:41:28 +0200

    Stretch flex 4 new Educ games

--- public/gameEducation.php
+++ public/gameEducation.php
@@ -18,49 +18,53 @@
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Flexbox Zombies</h3>
                     <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
-                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.
+                        <br>
                         <br>
                         <button class="fakePlayButton">PLAY</button>
                     </p>
                 </section>
             </a>
-            <!-- Cartouche block 1 end-->
-            <!-- Cartouche block 1 start-->
-            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <!-- Cartouche block 1 end-->
+                <!-- Cartouche block 2 start-->
+            <a href="http://flexboxfroggy.com/" target="_blank" class="cartoucheAnchor" title="Go to Flexbox Froggy website">
                 <section class="container gameCartouche">
-                    <h3 class="gameTittle">Flexbox Zombies</h3>
-                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
-                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                    <h3 class="gameTittle">Flexbox Froggy</h3>
+                    <img src="assets/images/game/flexboxfroggy.png" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <p class="gamePresentationText">Puzzle game where you have to help a group of colorful frogs get to their lillypads of choice. Moving the amphibians around is done with flexbox and will truly test you layout building skills. The in-game hints will tell you which CSS properties to use in each situation, but it's up to the player to decide what combination of values to set.
+                        <br>
                         <br>
                         <button class="fakePlayButton">PLAY</button>
                     </p>
                 </section>
             </a>
-            <!-- Cartouche block 1 end-->
-            <!-- Cartouche block 1 start-->
-            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <!-- Cartouche block 2 end-->
+                <!-- Cartouche block 3 start-->
+            <a href="https://flukeout.github.io/" target="_blank" class="cartoucheAnchor" title="Go to CSS Diner website">
                 <section class="container gameCartouche">
-                    <h3 class="gameTittle">Flexbox Zombies</h3>
-                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
-                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                    <h3 class="gameTittle">CSS Diner</h3>
+                    <img src="assets/images/game/cssdiner.png" class="gameIllustrationPicture"   alt="gameplay picture of CSS Diner">
+                    <p class="gamePresentationText">A great game for learning CSS selectors. The player is given a table of dishes in animated and HTML form, and a certain item or items to select from the table. The different levels cover everything a web developer should know about selectors, from the very basics to ~ and :first-child.
+                        <br>
                         <br>
                         <button class="fakePlayButton">PLAY</button>
                     </p>
                 </section>
             </a>
-            <!-- Cartouche block 1 end-->
-            <!-- Cartouche block 1 start-->
-            <a href="#" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
+                <!-- Cartouche block 3 end-->
+                <!-- Cartouche block 4 start-->
+            <a href="http://play.elevatorsaga.com/" target="_blank" class="cartoucheAnchor" title="Go to Elevator Saga website">
                 <section class="container gameCartouche">
-                    <h3 class="gameTittle">Flexbox Zombies</h3>
-                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
-                    <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.<br>
+                    <h3 class="gameTittle">Elevator Saga</h3>
+                    <img src="assets/images/game/elevator_saga.png" class="gameIllustrationPicture"   alt="gameplay picture of Elevator Saga">
+                    <p class="gamePresentationText">Elevator Saga is a puzzle game in which you use JavaScript to control the elevators in a building. Hordes of people are going up and down all the time and you have to try and get them to their destinations as quickly as possible. The game tests your algorithm writing abilities as well as your knowledge of JS functions, arrays and event handlers.
+                        <br>
                         <br>
                         <button class="fakePlayButton">PLAY</button>
                     </p>
                 </section>
             </a>
-            <!-- Cartouche block 1 end-->
+                <!-- Cartouche block 4 end-->
         </section>
     </main>
 <?php include('_footer.php'); ?>
