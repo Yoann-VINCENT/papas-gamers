d655e802e7cde5ad019cf5b95161ce5cfb65f847
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-02 21:48:31 +0200

    First section fullfil 5 new images for bored students

--- public/whatToDo.php
+++ public/whatToDo.php
@@ -11,6 +11,26 @@
 <?php include('assets/ressources/_header.php'); ?>
 <main>
     <h2 class="gameBigTittle">What to do ?</h2>
+    <!-- first solution -->
+    <section>
+        <h3>Learn PHP</h3>
+        <img src="assets/images/whatToDo/code.jpg">
+        <P>
+            PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
+            PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
+            PHP 7 is the latest stable release.
+        </P>
+    </section>
+    <!-- first solution -->
+    <section>
+        <h3>Practice some Yoga</h3>
+        <img src="assets/images/whatToDo/yoga.jpg">
+        <P>
+            PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
+            PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
+            PHP 7 is the latest stable release.
+        </P>
+    </section>
 </main>
 <?php include('assets/ressources/_footer.php'); ?>
 </body>
