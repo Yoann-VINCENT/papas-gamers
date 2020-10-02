d9b318de62e43046f2f2655fc9d7efdb38c38282
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-02 13:28:26 +0200

    Create whatToDo.php

---
+++ public/whatToDo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="assets/style/index.css">
+    <title>What to Do</title>
+</head>
+<body>
+<?php include('assets/ressources/_header.php'); ?>
+<main>
+    <h2 class="gameBigTittle">What to do ?</h2>
+</main>
+<?php include('assets/ressources/_footer.php'); ?>
+</body>
+</html>
