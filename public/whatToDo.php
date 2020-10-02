b9e6f8eff8814fd550b3927525342df8d69b80b4
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-02 22:57:39 +0200

    Background fixed font OK Cartouche OK

--- public/whatToDo.php
+++ public/whatToDo.php
@@ -5,33 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style/bootstrap.css">
     <link rel="stylesheet" href="assets/style/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
     <title>What to Do</title>
 </head>
-<body>
-<?php include('assets/ressources/_header.php'); ?>
-<main>
-    <h2 class="gameBigTittle">What to do ?</h2>
-    <!-- first solution -->
-    <section>
-        <h3>Learn PHP</h3>
-        <img src="assets/images/whatToDo/code.jpg">
-        <P>
-            PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
-            PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
-            PHP 7 is the latest stable release.
-        </P>
-    </section>
-    <!-- first solution -->
-    <section>
-        <h3>Practice some Yoga</h3>
-        <img src="assets/images/whatToDo/yoga.jpg">
-        <P>
-            PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
-            PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
-            PHP 7 is the latest stable release.
-        </P>
-    </section>
-</main>
-<?php include('assets/ressources/_footer.php'); ?>
+<body class="boredBackground">
+    <?php include('assets/ressources/_header.php'); ?>
+    <main class="boredPage">
+        <h2 class="gameBigTittle">What to do ?</h2>
+        <!-- first solution -->
+        <section class="boredCartouche">
+            <h3 class="boredTittle">Learn PHP</h3>
+            <img src="assets/images/whatToDo/code.jpg" class="boredPicture" alt="Someone coding on a computer">
+            <P class="boredDescription">
+                PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+                PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.
+                PHP 7 is the latest stable release.
+            </P>
+        </section>
+        <!-- 2nd solution -->
+        <section class="boredCartouche">
+            <h3 class="boredTittle">Practice some Yoga</h3>
+            <img src="assets/images/whatToDo/yoga.jpg" class="boredPicture" alt="Someone practicing yoga in front of a sunset">
+            <P class="boredDescription">
+                Yoga is a mind and body practice with a 5,000-year history in ancient Indian philosophy. Various styles of yoga combine physical postures, breathing techniques, and meditation or relaxation
+            </P>
+        </section>
+        <!-- 3rd solution -->
+        <section class="boredCartouche">
+            <h3 class="boredTittle">Practice some Yoga</h3>
+            <img src="assets/images/whatToDo/yoga.jpg" class="boredPicture" alt="Someone practicing yoga in front of a sunset">
+            <P class="boredDescription">
+                PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
+                PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
+                PHP 7 is the latest stable release.
+            </P>
+        </section>
+        <!-- 4th solution -->
+        <section class="boredCartouche">
+            <h3 class="boredTittle">Practice some Yoga</h3>
+            <img src="assets/images/whatToDo/yoga.jpg" class="boredPicture" alt="Someone practicing yoga in front of a sunset">
+            <P class="boredDescription">
+                PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
+                PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
+                PHP 7 is the latest stable release.
+            </P>
+        </section>
+        <!-- 5th solution -->
+        <section class="boredCartouche">
+            <h3 class="boredTittle">Practice some Yoga</h3>
+            <img src="assets/images/whatToDo/yoga.jpg" class="boredPicture" alt="Someone practicing yoga in front of a sunset">
+            <P class="boredDescription">
+                PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>
+                PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.<br>
+                PHP 7 is the latest stable release.
+            </P>
+        </section>
+    </main>
+    <?php include('assets/ressources/_footer.php'); ?>
+    <script src="assets/js/header.js"></script>
 </body>
 </html>
