54b19cc5aefc883f8a733239c7432af4e8cd4409
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-04 10:09:44 +0200

    Variables and DOM defined

---
+++ public/assets/js/clickPerSecond.js
@@ -0,0 +1,10 @@
+let score;
+let duration = 10;
+let startTime;
+let ended = true;
+
+let timerTxt = document.querySelector("#timer");
+let scoreTxt = document.querySelector("#score");
+let clicksTxt = document.querySelector("#clicks");
+let startBtn = document.querySelector("#start");
+let clickArea = document.querySelector("#clickarea");
