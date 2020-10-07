b24008fc1688bfbcfba8413fe197e8c978d0f0d9
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-10-07 14:06:41 +0200

    absolute path for all whatToDo, About, and games pages

--- public/gameArcade.php
+++ public/gameArcade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style/bootstrap.css">
-    <link rel="stylesheet" href="assets/style/index.css">
+    <link rel="stylesheet" href="/assets/style/bootstrap.css">
+    <link rel="stylesheet" href="/assets/style/index.css">
     <title>Arcade Games</title>
 </head>
 <body class="gameBackground">
-    <?php require_once('assets/ressources/_header.php'); ?>
+    <?php require_once('/assets/ressources/_header.php'); ?>
     <main>
         <h2 class="gameBigTittle">Arcade games</h2>
         <section  class="gamePage">
@@ -16,7 +16,7 @@
             <a href="whack-a-mole.php" target="_blank" class="cartoucheAnchor" title="Go to our Whac-A-Mole game">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Whac-A-Mole</h3>
-                    <img src="assets/images/game/whac-a-mole.jpg" class="gameIllustrationPicture"   alt="gameplay picture of our Whac-A-Mole game">
+                    <img src="/assets/images/game/whac-a-mole.jpg" class="gameIllustrationPicture"   alt="gameplay picture of our Whac-A-Mole game">
                     <p class="gamePresentationText">A typical Whac-A-Mole machine consists of a waist-level cabinet with a play area and display screen, and a large, soft, black mallet. Five holes in the play area top are filled with small plastic moles, which pop up at random. Points are scored by whacking each mole as it appears. The faster the reaction the higher the score.
                         <br>
                         <br>
@@ -29,7 +29,7 @@
             <a href="clickPerSecond.php" target="_blank" class="cartoucheAnchor" title="Go to our click per seconds game">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Frenzy clicker</h3>
-                    <img src="assets/images/game/click.png" class="gameIllustrationPicture"   alt="gameplay picture of our click per seconds game">
+                    <img src="/assets/images/game/click.png" class="gameIllustrationPicture"   alt="gameplay picture of our click per seconds game">
                     <p class="gamePresentationText">CPS or clicks per second represents a number of times that a person can click using their mouse during a specific period of time measured in seconds.
                         <br>
                         <br>
@@ -40,7 +40,7 @@
             <!-- Cartouche block 2 end-->
         </section>
     </main>
-    <?php require_once('assets/ressources/_footer.php'); ?>
-    <script src="assets/js/header.js"></script>
+    <?php require_once('/assets/ressources/_footer.php'); ?>
+    <script src="/assets/js/header.js"></script>
 </body>
 </html>
--- public/gameEducation.php
+++ public/gameEducation.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style/bootstrap.css">
-    <link rel="stylesheet" href="assets/style/index.css">
-    <title>Education Games</title>
+    <link rel="stylesheet" href="/assets/style/bootstrap.css">
+    <link rel="stylesheet" href="/assets/style/index.css">
+    <title>Educative Games</title>
 </head>
 <body class="gameBackground">
-    <?php include('assets/ressources/_header.php'); ?>
+    <?php require_once('/assets/ressources/_header.php'); ?>
     <main>
-        <h2 class="gameBigTittle">Education games</h2>
+        <h2 class="gameBigTittle">Educative games</h2>
         <section  class="gamePage">
                 <!-- Cartouche block 1 start-->
             <a href="https://mastery.games/flexboxzombies/" target="_blank" class="cartoucheAnchor" title="Go to flexbox-zombies website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Flexbox Zombies</h3>
-                    <img src="assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <img src="/assets/images/game/flexbox-zombies.jpg" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
                     <p class="gamePresentationText">Each section unravels part of the plot, gives you expertise over a new flexbox concept, and presents zombie survival challenges that force you to solidify your new skills like your life depends on it.
                         <br>
                         <br>
@@ -29,7 +29,7 @@
             <a href="http://flexboxfroggy.com/" target="_blank" class="cartoucheAnchor" title="Go to Flexbox Froggy website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Flexbox Froggy</h3>
-                    <img src="assets/images/game/flexboxfroggy.png" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
+                    <img src="/assets/images/game/flexboxfroggy.png" class="gameIllustrationPicture"   alt="gameplay picture of flexbox-zombies">
                     <p class="gamePresentationText">Puzzle game where you have to help a group of colorful frogs get to their lillypads of choice. Moving the amphibians around is done with flexbox and will truly test you layout building skills. The in-game hints will tell you which CSS properties to use in each situation, but it's up to the player to decide what combination of values to set.
                         <br>
                         <br>
@@ -42,7 +42,7 @@
             <a href="https://flukeout.github.io/" target="_blank" class="cartoucheAnchor" title="Go to CSS Diner website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">CSS Diner</h3>
-                    <img src="assets/images/game/cssdiner.png" class="gameIllustrationPicture"   alt="gameplay picture of CSS Diner">
+                    <img src="/assets/images/game/cssdiner.png" class="gameIllustrationPicture"   alt="gameplay picture of CSS Diner">
                     <p class="gamePresentationText">A great game for learning CSS selectors. The player is given a table of dishes in animated and HTML form, and a certain item or items to select from the table. The different levels cover everything a web developer should know about selectors, from the very basics to ~ and :first-child.
                         <br>
                         <br>
@@ -55,7 +55,7 @@
             <a href="http://play.elevatorsaga.com/" target="_blank" class="cartoucheAnchor" title="Go to Elevator Saga website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Elevator Saga</h3>
-                    <img src="assets/images/game/elevator_saga.png" class="gameIllustrationPicture"   alt="gameplay picture of Elevator Saga">
+                    <img src="/assets/images/game/elevator_saga.png" class="gameIllustrationPicture"   alt="gameplay picture of Elevator Saga">
                     <p class="gamePresentationText">Elevator Saga is a puzzle game in which you use JavaScript to control the elevators in a building. Hordes of people are going up and down all the time and you have to try and get them to their destinations as quickly as possible. The game tests your algorithm writing abilities as well as your knowledge of JS functions, arrays and event handlers.
                         <br>
                         <br>
@@ -68,7 +68,7 @@
             <a href="https://alexnisnevich.github.io/untrusted/" target="_blank" class="cartoucheAnchor" title="Go to Untrusted website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Untrusted</h3>
-                    <img src="assets/images/game/untrusted.jpg" class="gameIllustrationPicture"   alt="gameplay picture of Untrusted">
+                    <img src="/assets/images/game/untrusted.jpg" class="gameIllustrationPicture"   alt="gameplay picture of Untrusted">
                     <p class="gamePresentationText">Untrusted is a meta-JavaScript adventure game where you play the role of Dr. Eval, an @ sign that can control the world around him by changing the source code. The player is presented with the functions that initialize every level and has to alter them using the game API to create and escape route for Dr. Eval.
                         <br>
                         <br>
@@ -81,7 +81,7 @@
             <a href="http://www.hexinvaders.com/" target="_blank" class="cartoucheAnchor" title="Go to Hex Invaders website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Hex Invaders</h3>
-                    <img src="assets/images/game/hex-invaders.jpg" class="gameIllustrationPicture"   alt="gameplay picture of Hex Invaders">
+                    <img src="/assets/images/game/hex-invaders.jpg" class="gameIllustrationPicture"   alt="gameplay picture of Hex Invaders">
                     <p class="gamePresentationText">In this version of the classic arcade game the world is attacked by a trio of aliens, each sporting a different color. To save humanity, the player must quickly translate a RGB hex value into human colors and shoot down the invaders.
                         <br>
                         <br>
@@ -94,7 +94,7 @@
             <a href="https://cssgridgarden.com/" target="_blank" class="cartoucheAnchor" title="Go to Grid Garden website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Grid Garden</h3>
-                    <img src="assets/images/game/gridGarden.png" class="gameIllustrationPicture"   alt="gameplay picture of Grid Garden">
+                    <img src="/assets/images/game/gridGarden.png" class="gameIllustrationPicture"   alt="gameplay picture of Grid Garden">
                     <p class="gamePresentationText">Welcome to Grid Garden, where you write CSS code to grow your carrot garden! Water only the areas that have carrots by using the grid-column-start property.
                         <br>
                         <br>
@@ -107,7 +107,7 @@
             <a href="https://screeps.com/" target="_blank" class="cartoucheAnchor" title="Go to Screeps website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">Screeps</h3>
-                    <img src="assets/images/game/screeps.png" class="gameIllustrationPicture"   alt="gameplay picture of Screeps">
+                    <img src="/assets/images/game/screeps.png" class="gameIllustrationPicture"   alt="gameplay picture of Screeps">
                     <p class="gamePresentationText">You can think of this game as your digital ant farm where every "ant" is programmed by the player. After setting up a colony and the behavior of your creeps, they will go about their business of exploring the endless game world, collecting resources and fighting enemies even while you are offline.
                         <br>
                         <br>
@@ -120,7 +120,7 @@
             <a href="https://www.codingame.com/start" target="_blank" class="cartoucheAnchor" title="Go to CodinGame website">
                 <section class="container gameCartouche">
                     <h3 class="gameTittle">CodinGame</h3>
-                    <img src="assets/images/game/codinGame.jpeg" class="gameIllustrationPicture"   alt="gameplay picture of CodinGame">
+                    <img src="/assets/images/game/codinGame.jpeg" class="gameIllustrationPicture"   alt="gameplay picture of CodinGame">
                     <p class="gamePresentationText">A huge collection of games that can all be played in JavaScript, PHP, Ruby, Go, Python, and many other programming languages. The platform includes various puzzle, multiplayer and AI based games, while the code is written in an advanced IDE with Emacs and Vim support.
                         <br>
                         <br>
@@ -131,7 +131,7 @@
                 <!-- Cartouche block 9 end-->
         </section>
     </main>
-    <?php include('assets/ressources/_footer.php'); ?>
-    <script src="assets/js/header.js"></script>
+    <?php require_once('/assets/ressources/_footer.php'); ?>
+    <script src="/assets/js/header.js"></script>
 </body>
 </html>
