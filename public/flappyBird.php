ca2e8d335de3125a4fb2547e7111f8fdb875f580
Raphael Liere <raphael.liere@hotmail.fr>
2020-10-08 16:40:50 +0200

    FlappyBird game OK

--- public/flappyBird.php
+++ public/flappyBird.php
@@ -1,33 +1,29 @@
 <!doctype html>
-<html lang="en" onclick="jump()">
+<html lang="en" onkeypress="moveUp()">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="./assets/style/bootstrap.css" rel="stylesheet" type="text/css" >
-    <link href="./assets/style/index.css" type="text/css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/style/flappyBird.css">
-    <title>Flappy Bird</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="./assets/style/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="./assets/style/index.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/style/flappyBird.css">
+        <title>Flappy Bird</title>
+    </head>
 
-<body class="flappy_background">
+    <body class="flappyBirdBody">
 
-<?php require ('./assets/ressources/_header.php') ?>
+        <?php require ('./assets/ressources/_header.php') ?>
 
-<div id="game">
-    <div id="block"></div>
-    <div id="hole"></div>
-    <div id="character"></div>
-</div>
+        <h1 class="main_title">Flappy Bird</h1>
 
-<script src="assets/js/flappyBird.js"></script>
+        <canvas id="canvas" width="288" height="512" class="flappyBirdCanvas"></canvas>
 
-<?php include ('assets/ressources/_footer.php'); ?>
+        <?php include ('assets/ressources/_footer.php'); ?>
 
-</body>
+        <script src="assets/js/flappyBird.js"></script>
 
-
+    </body>
 
 </html>
